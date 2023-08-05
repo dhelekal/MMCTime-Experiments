fbc73da87554f504116cabf7ee880fdd452fd26e
David <2306427+dhelekal@users.noreply.github.com>
2023-08-05 11:18:19 +0100

    4th

---
+++ run/multi_sim_beta_iqtree/sim_set_beta.R
@@ -0,0 +1,25 @@
+library(ape)
+library(MMCTime)
+
+set.seed(1243695865)
+
+set_size <- 48L*3L
+samp_t <- 4:1
+n_d <- length(samp_t)
+n_tip <- 200
+
+nu <- 1/10
+
+alphas <-seq(from=.01, to=.99, length.out=set_size)
+
+for(i in 1:set_size)
+{
+    n_samp <- rmultinom(1, size=n_tip, prob=rep(1,n_d)/n_d)[,1]
+    stopifnot(sum(n_samp) == n_tip)
+    sim <- simulate_beta(samp_t, n_samp, nu, alphas[i])
+    write.tree(sim$phy, paste0("tree_",i,".nwk"))
+    write.table(sim$dates, paste0("dates_",i,".tab"))
+}
+
+
+
