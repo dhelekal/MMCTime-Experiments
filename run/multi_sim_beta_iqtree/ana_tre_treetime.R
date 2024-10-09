5fa6b09a34449e6c073afbaea70e6d83d940a09c
David <david.helekal@gmail.com>
2024-10-09 14:58:58 -0400

    tables and replots

--- run/multi_sim_beta_iqtree/ana_tre_treetime.R
+++ run/multi_sim_beta_iqtree/ana_tre_treetime.R
@@ -14,13 +14,8 @@ set.seed(index)
 
 tre_file <- paste0("tree_ml_",index,".nwk")
 tr_in <- read.tree(tre_file)
-tr_in <- multi2di(tr_in)
-
 n_tip <- length(tr_in$tip.label)
 
-#pos <- sample(1:(n_tip - 2),1)
-#outgroup <- ifelse(pos > n_tip, extract.clade(tr_in, pos)$tip.label, tr_in$tip.label[pos])
-#tr_in <- root(tr_in, outgroup=outgroup, resolve.root=T)
 tr_in$edge.length <- tr_in$edge.length / 10000L
 
 dates_tab <- read.table(paste0("../gt/dates_", index , ".tab"))
@@ -36,4 +31,4 @@ write.tree(tr_in, tfile)
 
 afile <- paste0("seqs_", index, ".nex")
 
-system(paste0("treetime --tree '", tfile ,"' --dates '", dfile ,"' --aln '", afile, "' --outdir '", outdir, "' --reroot 'min_dev' --max-iter 150 --relax 5.0 0 --time-marginal false --rng-seed ", index))
+system(paste0("treetime --tree '", tfile ,"' --dates '", dfile ,"' --aln '", afile, "' --outdir '", outdir, "' --reroot 'min_dev' --coalescent const --max-iter 150 --relax 5.0 0 --time-marginal false --rng-seed ", index))
